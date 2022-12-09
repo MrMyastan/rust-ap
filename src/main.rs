14b31e7f16566b469d7e39da2afa9af254495c83
MrMyastan <53984236+MrMyastan@users.noreply.github.com>
2022-12-09 13:40:57 -0600

    yolo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rocket::fs::NamedFile;
+
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -5,7 +7,22 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+// #[get("/.well-known/webfinger?<resource>")]
+// fn acct(resource: &str) -> Option<content::RawJson<String>> {
+//     fs::read_to_string(format!("{resource}.json")).ok().map(|s| content::RawJson(s))
+// }
+
+#[get("/actor")]
+async fn actor() -> Option<NamedFile> {
+    NamedFile::open("actor.json").await.ok()
+}
+
+#[get("/.well-known/webfinger?resource=acct:css@apub.skweekychair.games")] 
+async fn acct() -> Option<NamedFile> {
+    NamedFile::open("acct.json").await.ok()
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, actor, acct])
 }
